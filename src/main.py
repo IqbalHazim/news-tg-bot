dbc6320dec8809dbb2c55d889b1352fd3cd42c1a
IqbalHzm <iqbal@defidive.com>
2025-06-13 17:45:27 +0800

    Added logging and proceed to send json as poc

--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
 import json
-
+import logging
+from logging.config import fileConfig
 from services.scraping_service import  ScrapingService
 from services.ai_service import CryptoNewsSummarizer
 from services.bot_service import BotService
 
-from config import DEEPSEEK_API_KEY
+from config import DEEPSEEK_API_KEY, DEFIDIVE_API_URL, USER_AGENT, LANGUAGE, SENTENCES_COUNT
 
-DEFIDIVE_API_URL = "https://api.defidive.com"
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-LANGUAGE = "english"
-SENTENCES_COUNT = 10
-
+fileConfig('src/utils/logging_config.ini')
+logger = logging.getLogger(__name__)
+    
 if __name__ == "__main__":
     llm_config = {
         "api_key": DEEPSEEK_API_KEY,
@@ -31,7 +30,12 @@ if __name__ == "__main__":
     )
 
     bot = BotService(scraping_service=scraper, ai_service=summarizer)
-    print("Starting the scraping and summarization process...")
+    logger.info("Starting the scraping and summarization process...")
     summary = bot.start_summarization()
-    print("Summary generated successfully:")
-    print(json.dumps(summary, indent=2, ensure_ascii=False))
+    logger.info("Summary generated successfully!")
+
+    # for debugging
+    # print(json.dumps(summary, indent=2, ensure_ascii=False))
+
+    cleaned_summary_json = summary.replace("/ ", "").replace("/n ", "").strip()
+    bot.send_to_channel(cleaned_summary_json)
