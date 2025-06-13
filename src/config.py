625903423919722cbf322e805df53b2702a8ec6f
IqbalHzm <iqbal@defidive.com>
2025-06-13 17:44:26 +0800

    Moved several constants here

--- src/config.py
+++ src/config.py
@@ -1,24 +1,36 @@
 import os
+import logging
+from logging.config import fileConfig
 from dotenv import load_dotenv, find_dotenv
+
+fileConfig('src/utils/logging_config.ini')
+logger = logging.getLogger(__name__)
 
 # Load environment
 env_file = find_dotenv()
 if env_file:
     load_dotenv(env_file, override=True)
 
-# Required configuration
-# BOT_TOKEN = os.getenv('BOT_TOKEN')
-# if not BOT_TOKEN:
-#     raise ValueError("BOT_TOKEN required in .env file")
+DEFIDIVE_API_URL = "https://api.defidive.com"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+LANGUAGE = "english"
+SENTENCES_COUNT = 10
 
+# Required configuration
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+if not TELEGRAM_BOT_TOKEN:
+    raise ValueError("TELEGRAM_BOT_TOKEN required in .env file")
+if TELEGRAM_BOT_TOKEN:
+    logger.info(f"✓ BOT_TOKEN loaded")
+else:
+    logger.error("⚠️ Warning: DEEPSEEK_API_KEY not set. AI summary will be disabled.")
+    
 # Optional but recommended for synopsis generation
 DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY')
-if not DEEPSEEK_API_KEY:
-    print("⚠️ Warning: DEEPSEEK_API_KEY not set. AI summary will be disabled.")
+if DEEPSEEK_API_KEY:
+    logger.info(f"✓ DEEPSEEK_API_KEY loaded")
+else:
+    logger.error("⚠️ Warning: DEEPSEEK_API_KEY not set. AI summary will be disabled.")
 
 # Logging
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
-
-print(f"✓ BOT_TOKEN loaded")
-if DEEPSEEK_API_KEY:
-    print(f"✓ Deepseek API key loaded")
