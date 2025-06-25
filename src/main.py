8d0eb2384688e268ea0a474fc3cd904a147e9b5e
IqbalHzm <iqbal@defidive.com>
2025-06-25 16:59:49 +0800

    Test prediction and plotting feature

--- src/main.py
+++ src/main.py
@@ -1,33 +1,40 @@
 import logging
 from apscheduler.schedulers.background import BackgroundScheduler
-# from apscheduler.schedulers.blocking import BlockingScheduler
 from logging.config import fileConfig
-
+import matplotlib.pyplot as plt
 
 fileConfig('src/utils/logging_config.ini')
 logger = logging.getLogger(__name__)
 
-# from scheduler import schedule_summary
-from services.price_service import PriceService
-if __name__ == "__main__":
-    # scheduler = BackgroundScheduler()
-    # scheduler.add_job(schedule_summary, 'interval', hours=6)
-    # scheduler.start()
+from scheduler import schedule_summary
+from predictor import train_and_predict_data, test_fetch_price, plot_price_with_sma
 
-    # logger.info("Scheduler started. Waiting for jobs to run...")
+if __name__ == "__main__":
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(schedule_summary, 'interval', hours=6)
+    scheduler.start()
+
+    logger.info("Scheduler started. Waiting for jobs to run...")
     
-    # try:
-    #     # Keep the script running to allow the scheduler to run jobs
-    #     while True:
-    #         pass
-    # except (KeyboardInterrupt, SystemExit):
-    #     scheduler.shutdown()
-    #     logger.info("Scheduler stopped.")
-    logger.info("Starting the PriceService to fetch top tokens...")
-    price_service = PriceService()
-    tokens = price_service.get_top_tokens(limit=3, page="1", sort_by="rank", sort_order="asc")
-    logger.info("Top tokens fetched successfully: %s", tokens)
-    logger.info("Fetching prices for top tokens...")
-    for token in tokens:
-        price = price_service.get_price(token)
-        logger.info("Price for %s: \n %s", token, price)
+    try:
+        # Keep the script running to allow the scheduler to run jobs
+        while True:
+            pass
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+        logger.info("Scheduler stopped.")
+
+    # # === Test predict and plot price data for top tokens ===
+    # tokens = test_fetch_price()
+
+    # for token in tokens:
+    #     # To get prediction for the next 6 hours
+    #     symbol = token.get('token', "")
+    #     logger.info(f"Training model for {symbol} prediction...")
+        
+    #     price_data = token.get('price_data', {})
+    #     model, prediction = train_and_predict_data(symbol, price_data)
+
+    #     # To get plot with moving averages
+    #     plot_price_with_sma(token['price_data'], title=f"{symbol} Price with Moving Averages")
+    #     plt.show()
