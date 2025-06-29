3efbb1348553c6b25b24cf65f3378ac3f2e50b8a
IqbalHzm <iqbal@defidive.com>
2025-06-29 23:07:48 +0800

    remove scheduler

--- src/main.py
+++ src/main.py
@@ -1,25 +1,46 @@
 import logging
-from apscheduler.schedulers.background import BackgroundScheduler
-# from apscheduler.schedulers.blocking import BlockingScheduler
 from logging.config import fileConfig
-
 
 fileConfig('src/utils/logging_config.ini')
 logger = logging.getLogger(__name__)
 
-from scheduler import schedule_summary
+from services.scraping_service import  ScrapingService
+from services.ai_service import CryptoNewsSummarizer
+from services.bot_service import BotService
+
+from config import DEEPSEEK_API_KEY, DEFIDIVE_API_URL, USER_AGENT, LANGUAGE, SENTENCES_COUNT
+
 
 if __name__ == "__main__":
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(schedule_summary, 'interval', hours=6)
-    scheduler.start()
-
-    logger.info("Scheduler started. Waiting for jobs to run...")
+    logger.info("Cron job active. Jobs run every 6 hours...")
     
+    logger.info("Job STARTED")
+    llm_config = {
+        "api_key": DEEPSEEK_API_KEY,
+    }
+
+    scraper = ScrapingService(
+        defidive_api_url=DEFIDIVE_API_URL,
+        user_agent=USER_AGENT,
+        language=LANGUAGE,
+        sentences_count=SENTENCES_COUNT
+    )
+
+    # Initialize the summarizer with OpenAI
+    summarizer = CryptoNewsSummarizer(
+        llm_provider="openai",
+        llm_config=llm_config,
+        model_name="deepseek-chat"
+    )
+
+    bot = BotService(scraping_service=scraper, ai_service=summarizer)
+    logger.info("Starting the scraping and summarization process...")
+
     try:
-        # Keep the script running to allow the scheduler to run jobs
-        while True:
-            pass
-    except (KeyboardInterrupt, SystemExit):
-        scheduler.shutdown()
-        logger.info("Scheduler stopped.")
+        summary = bot.start_summarization()
+        logger.info("Summary generated successfully!")
+        
+        cleaned_summary_json = scraper.clean_text(summary)
+        bot.send_to_channel(cleaned_summary_json)
+    except Exception as e:
+        logger.error(f"Failed to send summary: {str(e)}", exc_info=True)
